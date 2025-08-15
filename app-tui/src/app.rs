824fcc890ea8fee198de94ca4bc6082f82fe4fc1
moko256 <koutaro.mo@gmail.com>
2025-08-15 22:59:55 +0900

    feat(todo): add vim-like style to tui

--- app-tui/src/app.rs
+++ app-tui/src/app.rs
@@ -5,8 +5,7 @@ use ratatui::{
     DefaultTerminal, Frame,
     layout::{Constraint, Layout},
     style::Stylize,
-    text::Line,
-    widgets::{Block, Paragraph},
+    widgets::Paragraph,
 };
 
 #[derive(Debug, Default)]
@@ -42,29 +41,36 @@ impl App {
 
     fn render(&mut self, frame: &mut Frame) {
         let vertical = Layout::vertical([
-            Constraint::Length(3),
             Constraint::Min(1),
             Constraint::Length(1),
+            Constraint::Length(1),
         ]);
-        let [input_area, todos_area, footer_area] = vertical.areas(frame.area());
+        let [todos_area, footer_area, input_area] = vertical.areas(frame.area());
 
-        let input_title = Line::from("New todo:").bold();
+        let buffer_height = todos_area.height;
+        let list_height = self.state.todos.len() as u16;
+        if buffer_height > list_height {
+            let empty_placeholder_height = buffer_height - list_height;
+            let empty_placeholder_top = todos_area.top() + buffer_height - empty_placeholder_height;
+
+            for top in empty_placeholder_top..empty_placeholder_top + empty_placeholder_height {
+                let mut placeholder_line_area = Clone::clone(&todos_area);
+                placeholder_line_area.y = top;
+
+                frame.render_widget(Paragraph::new("~"), placeholder_line_area);
+            }
+        }
+
+        let todos_text = self.state.todos.join("\n");
+        frame.render_widget(Paragraph::new(todos_text), todos_area);
+
+        let footer_text: &'static str = "[Todos] Press `Esc`or `Ctrl-C` to stop running.";
+        frame.render_widget(Paragraph::new(footer_text).reversed().bold(), footer_area);
+
         frame.render_widget(
-            Paragraph::new(self.state.todo_content_add.as_str())
-                .block(Block::bordered().title(input_title))
-                .yellow(),
+            Paragraph::new(format!("New todo: {}", self.state.todo_content_add)),
             input_area,
         );
-
-        let todos_title = Line::from("Todos:").bold();
-        let todos_text = self.state.todos.join("\n");
-        frame.render_widget(
-            Paragraph::new(todos_text).block(Block::bordered().title(todos_title)),
-            todos_area,
-        );
-
-        let footer_text: &'static str = "Press `Esc`or `Ctrl-C` to stop running.";
-        frame.render_widget(Paragraph::new(footer_text), footer_area);
     }
 
     fn handle_crossterm_events(&mut self) -> Result<()> {
